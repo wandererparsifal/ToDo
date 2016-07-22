f1cd5184b3ac900cea2e930d406ac42e7ecf0109
yangming <wandererparsifal@gmail.com>
2016-07-22 13:47:24 +0800

    add Application

---
+++ app/src/main/java/com/parsifal/todo/ToDoApplication.java
@@ -0,0 +1,14 @@
+package com.parsifal.todo;
+
+import android.app.Application;
+
+/**
+ * Created by YangMing on 2016/7/22 13:44.
+ */
+public class ToDoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
