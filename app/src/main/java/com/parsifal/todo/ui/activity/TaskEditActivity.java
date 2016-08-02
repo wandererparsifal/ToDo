40bcbf89ed64d51346fdd8f4063ab1df4c14371e
YangMing <wandererparsifal@gmail.com>
2016-08-02 17:00:25 +0800

    add taskedit toolbar

--- app/src/main/java/com/parsifal/todo/ui/activity/TaskEditActivity.java
+++ app/src/main/java/com/parsifal/todo/ui/activity/TaskEditActivity.java
@@ -1,19 +1,54 @@
 package com.parsifal.todo.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.parsifal.todo.R;
 
 /**
  * Created by YangMing on 2016/7/28.
  */
-public class TaskEditActivity extends Activity {
+public class TaskEditActivity extends AppCompatActivity {
+
+    private Toolbar toolbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_edit);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_task_edit);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.vector_drawable_arrow_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_edit_done:
+
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.taskedit, menu);
+        return true;
     }
 }
