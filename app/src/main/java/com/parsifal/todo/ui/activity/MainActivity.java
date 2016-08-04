20a40073b4251eaddff1fe7b624e421f4e311775
yangming <wandererparsifal@gmail.com>
2016-08-04 17:02:40 +0800

    not use ActionBarDrawerToggle

--- app/src/main/java/com/parsifal/todo/ui/activity/MainActivity.java
+++ app/src/main/java/com/parsifal/todo/ui/activity/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -20,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = null;
     FloatingActionButton fab = null;
     DrawerLayout drawer = null;
-    ActionBarDrawerToggle toggle = null;
     NavigationView navigationView = null;
 
     @Override
@@ -29,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.app_bar_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.vector_drawable_menu);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    drawer.openDrawer(GravityCompat.START);
+                }
+            }
+        });
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -39,27 +48,29 @@ public class MainActivity extends AppCompatActivity {
         });
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
             @Override
             public void onDrawerOpened(View drawerView) {
-                toolbar.setTitle("Open");
-                super.onDrawerOpened(drawerView);
+                toolbar.setTitle("Close");
+                toolbar.setNavigationIcon(R.drawable.vector_drawable_arrow_back);
             }
 
             @Override
             public void onDrawerClosed(View drawerView) {
-                toolbar.setTitle("Close");
-                super.onDrawerClosed(drawerView);
+                toolbar.setTitle("Open");
+                toolbar.setNavigationIcon(R.drawable.vector_drawable_menu);
             }
 
             @Override
-            public boolean onOptionsItemSelected(MenuItem item) {
-                return super.onOptionsItemSelected(item);
+            public void onDrawerStateChanged(int newState) {
+
             }
-        };
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        });
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
